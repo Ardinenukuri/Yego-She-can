1301f7171f6d78440dd23143ae986d473dd956c3
Afua Hamissi <lyhamissi@example.com>
2025-07-12 23:41:48 +0200

    refined navbar design and added footer , login and register pages

--- Website/frontend/next.config.ts
+++ Website/frontend/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['http://192.168.43.229:3000']
+
 };
 
 export default nextConfig;
