a63cab6212ac5fd7a41a388d83b932e8206a74c9
Ardinenukuri <a.nukuri@alustudent.com>
2025-08-12 13:36:20 +0200

    vercel

--- Website/frontend/next.config.ts
+++ Website/frontend/next.config.ts
@@ -14,20 +14,11 @@ const nextConfig: NextConfig = {
     ],
   },
   allowedDevOrigins: ['http://192.168.43.229:3000'],
-<<<<<<< HEAD
   output: 'standalone',
   typescript:{
     ignoreBuildErrors: true
 
   }
-=======
-
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
->>>>>>> 641ce5d80a48408d3b2b76c2b6cce6b078275423
 };
 
 export default nextConfig;
